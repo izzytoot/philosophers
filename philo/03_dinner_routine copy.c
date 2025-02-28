ccd637eebc96acc02dbdd5c03d831e12e55c6fac
izzytoot <isabeltootill@gmail.com>
2025-02-28 16:17:49 +0000

    best version again

--- philo/03_dinner_routine copy.c
+++ philo/03_dinner_routine copy.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,10 +7,10 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 18:27:17 by root              #+#    #+#             */
-/*   Updated: 2025/02/28 15:36:43 by root             ###   ########.fr       */
+/*   Updated: 2025/02/28 15:47:31 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+/*
 #include "philo.h"
 
 void	*mr_lonely(void *ph_ptr)
@@ -69,12 +70,13 @@ void	*monitor(void *ph_ptr)
 		}
 		my_usleep(philo->data, 100);
 	}
-	return (NULL);
+	return(false);
 }
 
 void	*dinner_routine(void *ph_ptr)
 {
 	t_philo *philo;
+//	long	current_time;
 	
 	philo = (t_philo *)ph_ptr;
 	wait_threads(philo->data);
@@ -94,7 +96,7 @@ void	start_dinner(t_data *data)
 	int	i;
 	
 	set_time_var(data, &data->data_mtx, &data->start_meal_time, get_time(data, MILLISECONDS));
-	handle_thread(data, &data->mon_thread, &monitor, &data->ph[0], CREATE);
+	handle_thread(data, &data->mon_thread, &monitor, &data, CREATE);
 	if (data->nb_ph == 1)
 	{	
 		handle_thread(data, &data->data_thread, &mr_lonely, &data->ph[0], CREATE);
@@ -112,3 +114,5 @@ void	start_dinner(t_data *data)
 		handle_thread(data, &data->ph[i].ph_thread, NULL, NULL, JOIN);
 	handle_thread(data, &data->mon_thread, NULL, NULL, JOIN);
 }
+
+*/
